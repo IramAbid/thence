a620f780bcaf1e59f3ca5f75b0a50453b4658f2a
IramAbid <irama4633@gmail.com>
2024-05-11 03:01:31 +0530

    Made final changes

--- brunel/src/components/navbar/navbar.jsx
+++ brunel/src/components/navbar/navbar.jsx
@@ -5,14 +5,17 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
     return (
-        <div className={styles.navcontainer}>
-        <nav className={styles.navbar}>
+        <div className={styles.nav}>
+            <div className={styles.navcontainer}>
+            <div className={styles.navbar}>
             <img src={logo} alt='logo' className={styles.logo}/>
             <div className={styles.buttoncontainer}>
-                <div className={styles.button1}><Link to="/register" style={{ color: '#1C1C1C', textDecoration: 'none' }}><p className={styles.buttontext}>Get Project</p></Link></div>
-                <button className={styles.button2}><p className={styles.buttontext}>Talent Onboard</p></button>
+            <div className={styles.button1}><Link to="/register" style={{ color: '#1C1C1C', textDecoration: 'none' }}><p className={styles.buttontext}>Get Project</p></Link></div>
+            <button className={styles.button2}><p className={styles.buttontext}>Talent Onboard</p></button>
             </div>
-        </nav>
+
+        </div>
+        </div>
         </div>
     );
 }
